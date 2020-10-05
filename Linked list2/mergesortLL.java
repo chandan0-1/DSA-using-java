3dbbafb3b0f6081ea81f2c976665d06196621de9
kingofbbsr <chandanofficial.cn@gmail.com>
2020-10-05 22:55:36 +0530

    Merge sort in Linked list

---
+++ Linked list2/mergesortLL.java
@@ -0,0 +1,59 @@
+/*
+
+    Following is the Node class already written for the Linked List
+
+    class LinkedListNode<T> {
+        T data;
+        LinkedListNode<T> next;
+    
+        public LinkedListNode(T data) {
+            this.data = data;
+        }
+    }
+
+*/
+
+public class Solution {
+    public static LinkedListNode<Integer> merge(LinkedListNode<Integer> h1,LinkedListNode<Integer> h2){
+        LinkedListNode<Integer> cur=null;
+        if(h1==null){
+            return h2;
+        }
+        if (h2==null){
+            return h1;
+        }
+        if (h1.data<h2.data){
+            cur=h1;
+            cur.next=merge(h1.next,h2);
+        }
+        else{
+            cur=h2;
+            cur.next=merge(h1,h2.next);
+        }
+        return cur;
+    }
+    
+    public static LinkedListNode<Integer> Mid(LinkedListNode<Integer> head){
+        LinkedListNode<Integer> s=head;
+        LinkedListNode<Integer> f=head;
+        while(f.next!=null && f.next.next!=null){
+            s=s.next;
+            f=f.next.next;
+        }
+        LinkedListNode<Integer> mid=s.next;
+        s.next=null;
+        return mid;
+    }
+	public static LinkedListNode<Integer> mergeSort(LinkedListNode<Integer> head) {
+		if (head==null || head.next==null){
+            return head;
+        }
+        LinkedListNode<Integer> mid=Mid(head);
+        // mid.next=null;
+        LinkedListNode<Integer> L1=mergeSort(head);
+        LinkedListNode<Integer> L2=mergeSort(mid);
+        LinkedListNode<Integer> l=merge(L1,L2);
+        return l;
+	}
+
+}
