b90e760dea7a7959bd3ffd03c69d5eaa3426887b
kingofbbsr <chandanofficial.cn@gmail.com>
2020-11-05 21:33:49 +0530

    LCA of BST

---
+++ BST 1/LCAofBST.java
@@ -0,0 +1,29 @@
+public class Solution {
+
+	/*
+	 * Binary Tree Node class
+	 * 
+	 * class BinaryTreeNode<T> { T data; BinaryTreeNode<T> left; BinaryTreeNode<T> right;
+	 * 
+	 * public BinaryTreeNode(T data) { this.data = data; } }
+	 */
+
+
+	
+	public static int getLCA(BinaryTreeNode<Integer> root, int a, int b) {
+        if (root==null){
+            return -1;
+        }
+        
+        if (root.data>a && root.data>b){
+            return getLCA(root.left,a,b);
+        }
+        else if (root.data<Math.min(a,b)){
+            return getLCA(root.right,a,b);
+        }
+        else{
+            return root.data;
+        }
+		
+	}
+}
