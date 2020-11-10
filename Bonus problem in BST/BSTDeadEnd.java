ee6d4004ba8024d57b24145dd7aa34a4ab3c7db7
kingofbbsr <chandanofficial.cn@gmail.com>
2020-11-10 23:36:39 +0530

    BSTDeadEnd.java

---
+++ Bonus problem in BST/BSTDeadEnd.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Solution {
+
+    /*	Binary Tree Node class
+
+       class BinaryTreeNode<T> {
+            T data;
+            BinaryTreeNode<T> left;
+            BinaryTreeNode<T> right;
+
+            public BinaryTreeNode(T data) {
+                this.data = data;
+            }
+        }
+
+    */
+    public static boolean helper(BinaryTreeNode<Integer> root,int min,int max) {
+        if(root==null)
+        {
+            return false;
+        }
+        if(min==max){
+            return true;
+        }
+        return helper(root.left,min,root.data-1) || helper(root.right,root.data+1,max);
+    }
+    public static boolean deadEnd(BinaryTreeNode<Integer> root) {
+        return helper(root,0,9999999);
+    }
+
+}
+
